56ee690f5bcf5bcd5772c6d4a7a389f681f2906e
Alfie <torcixld@gmail.com>
2019-11-06 16:11:27 +0000

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ echo
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<title>Praysu!</title>
+	<title>Praysu, the server for christians</title>
 	
 	<meta name="description" content="A Holy osu! Server">
 	<meta property="og:title" content="Circle Clicking & Praying">
